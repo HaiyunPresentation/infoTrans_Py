c7f674b57139040b4b9f33f033441bbcbe22bb9c
Nonoor <50571420+Nonoor@users.noreply.github.com>
2020-03-13 14:36:01 +0800

    Improved encoding and decoding

---
+++ decodeCL.py
@@ -0,0 +1,278 @@
+import numpy as np
+import cv2
+import struct
+
+width = 72
+inWidth = 40
+locWidth = 16
+border = 4
+
+sLocWidth = 8
+sblackWhite = 7
+
+def getContours(image):
+	gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+	#gray = cv2.blur(gray, (5, 5), 0)
+	ret,gray=cv2.threshold(gray,100,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+	contours,hierachy=cv2.findContours(gray,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	cv2.imwrite("out.png",gray)
+	cv2.waitKey()
+	return gray,contours,hierachy
+	#cv2.drawContours(img,contours,-1,(0,0,255),3)
+	#cv2.imwrite("output.png",img)
+
+def computeRate1(contours,i,j):
+	area1 = cv2.contourArea(contours[i])
+	area2 = cv2.contourArea(contours[j])
+	if area2 == 0:
+		return False
+	ratio = area1*1.0/area2
+	if abs(ratio-49.0/25) < 1:
+		return True
+	return False
+
+def computeRate2(contours,i,j):
+	area1 = cv2.contourArea(contours[i])
+	area2 = cv2.contourArea(contours[j])
+	if area2 == 0:
+		return False
+	ratio = area1*1.0/area2
+	if abs(ratio-25.0/9) < 1:
+		return True
+	return False
+
+def getCenter(contours,i):
+	M=cv2.moments(contours[i])
+	x = (M['m10']/M['m00'])
+	y = (M['m01']/M['m00'])
+	return x,y
+
+def detectContours(vec):
+	distance1=np.sqrt((vec[0]-vec[2])**2+(vec[1]-vec[3])**2)
+	distance2=np.sqrt((vec[0]-vec[4])**2+(vec[1]-vec[5])**2)
+	distance3=np.sqrt((vec[2]-vec[4])**2+(vec[3]-vec[5])**2)
+	if sum((distance1,distance2,distance3))/3<1:
+		return True
+	return False
+
+def judgeOrder(rec):
+	if len(rec)<4:
+		print("not find enough diction point!")
+		return -1,-1,-1,-1
+	max = 0
+	index = 0
+	for i in range(len(rec)):
+		if(rec[i][0]>max):
+			max = rec[i][0]
+			index = i
+	for i in range(len(rec)):
+		if i == index:
+			continue
+		for j in range(i+1,len(rec)):
+			if j == index:
+				continue
+			for k in range(j+1,len(rec)):
+				if k == index:
+					continue
+				distance1 = np.sqrt((rec[i][0] - rec[j][0]) ** 2 + (rec[i][1] - rec[j][1]) ** 2)
+				distance2 = np.sqrt((rec[i][0] - rec[k][0]) ** 2 + (rec[i][1] - rec[k][1]) ** 2)
+				distance3 = np.sqrt((rec[j][0] - rec[k][0]) ** 2 + (rec[j][1] - rec[k][1]) ** 2)
+				if abs(distance1-distance2)/10<5:
+					if abs(np.square(distance1)+np.square(distance2)- np.square(distance3))/(2*distance1*distance2) < 0.1:
+						if rec[j][0]<rec[k][0]:
+							return i,j,index,k
+						else:
+							return i,k,index,j
+				elif abs(distance1-distance3)/10<5:
+					if abs(np.square(distance1)+np.square(distance3)- np.square(distance2))/(2*distance1*distance3) < 0.1:
+						if rec[i][0]<rec[k][0]:
+							return j,i,index,k
+						else:
+							return j,k,index,i
+				elif abs(distance2-distance3)/10<5:
+					if abs(np.square(distance2)+np.square(distance3)- np.square(distance1))/(2*distance2*distance3) < 0.1:
+						if rec[i][0]<rec[j][0]:
+							return k,i,index,j
+						else:
+							return k,j,index,i
+	return -1,-1,-1,-1
+
+def find(image,contours,hierachy,root=0):
+	rec=[]
+	for i in range(len(hierachy)):
+		child = hierachy[i][2]
+		grandchild = hierachy[child][2]
+		if child!=-1 and grandchild !=-1:
+			if computeRate1(contours,i,child) and computeRate2(contours,child,grandchild):
+				x1,y1 = getCenter(contours,i)
+				x2,y2 = getCenter(contours,child)
+				x3,y3 = getCenter(contours,grandchild)
+				if detectContours([x1,y1,x2,y2,x3,y3,i,child,grandchild]):
+					rec.append([x1,y1,x2,y2,x3,y3,i,child,grandchild])
+	i,j,k,t = judgeOrder(rec)
+	if i==-1 or j==-1 or k ==-1 or t==-1:
+		print("not find enough anchor point")
+		return 
+
+	#ts = np.concatenate((contours[rec[i][6]],contours[rec[j][6]],contours[rec[k][6]]))
+	#rect = cv2.minAreaRect(ts)
+	#box = cv2.boxPoints(rect)
+	#box = np.int32(box)
+	vertexSrc = np.array([[rec[i][0],rec[i][1]],[rec[j][0],rec[j][1]],[rec[k][0],rec[k][1]],[rec[t][0],rec[t][1]]],dtype="float32")
+	#print(vertexSrc)
+	vertexWarp=np.array([[69.5,69.5],[69.5,649.5],[684.5,684.5],[649.5,69.5]],dtype="float32")
+	M = cv2.getPerspectiveTransform(vertexSrc,vertexWarp)
+	out = cv2.warpPerspective(image,M,(width*10,width*10))
+
+	#rect = four_point_transform(image, box)
+	#out = cv2.resize(rect,(width*10,width*10))
+	#box = cv2.boxPoints(rect)
+	#cv2.drawContours(image,[box],0,(0,0,255),2)
+	#cv2.imwrite("output.jpg",image)
+	#out = image[box[1][0]:box[3][0],box[1][1]:box[3][1]]
+	return out
+
+def decode(image):
+	mat = [[0.0 for i in range(width)]for j in range(width)]
+	pwidth = 10
+	i = 0
+	for i in range(width*10):
+		normali = i/pwidth
+		for j in range(width*10):
+			normalj = j/pwidth
+			if(normali<len(mat) and normalj<len(mat)):
+				#加权
+				if i%10<3 or i%10>6 or j%10<3 or j%10>6:
+					mat[int(normali)][int(normalj)]+=image[i][j]*0.2/84
+				else:
+					mat[int(normali)][int(normalj)]+=image[i][j]*0.05
+				#if i%10==4 and j%10==4:
+				#	mat[int(normali)][int(normalj)]=image[i][j]
+	#print(image[5][91])
+	#print(mat[0][9])
+	#转二进制
+	binstring=""
+	row=0
+	thre=127.5
+	col=locWidth #
+	while row<width:
+		if row<locWidth:
+			if (row+col)% 2==0:
+				if(mat[row][col] > thre):
+					mat[row][col] = 0
+				else:
+					mat[row][col] = 255
+			if mat[row][col] > thre:
+				binstring+="0"
+			else:
+				binstring+="1"
+			col += 1
+			if col>width-locWidth-1:
+				if row!=locWidth-1:
+					col = locWidth
+				else:
+					col = 0
+				row += 1
+		elif row<width-locWidth:
+			if (row+col)% 2==0:
+				if(mat[row][col] > thre):
+					mat[row][col] = 0
+				else:
+					mat[row][col] = 255
+			if mat[row][col] > thre:
+				binstring+="0"
+			else:
+				binstring+="1"
+			col += 1
+			if(col>width-1):
+				if row !=width-locWidth-1:
+					col = 0
+				else:
+					col = locWidth
+				row += 1
+		elif row < width-sLocWidth:
+			if (row+col)% 2==0:
+				if(mat[row][col] > thre):
+					mat[row][col] = 0
+				else:
+					mat[row][col] = 255
+			if mat[row][col] > thre:
+				binstring+="0"
+			else:
+				binstring+="1"
+			col += 1
+			if col>width-1:
+				col = locWidth
+				row += 1
+		else:
+			if (row+col)% 2==0:
+				if(mat[row][col] > thre):
+					mat[row][col] = 0
+				else:
+					mat[row][col] = 255
+			if mat[row][col] > thre:
+				binstring+="0"
+			else:
+				binstring+="1"
+			col += 1
+			if col>width-sLocWidth-1:
+				col = locWidth
+				row += 1
+	#res=""
+	#print(binstring)
+	#while binstring:
+	#	t = (int(binstring[:8],2))
+	#	res += chr(t)
+	#	binstring = binstring[8:]
+	#res = ""
+	#print(binstring)
+	writer = open("./output/output.txt",'wb+')
+	while binstring:
+		t = (int(binstring[:8],2))
+		res = struct.pack('B', t)
+		writer.write(res)
+		binstring = binstring[8:]
+	writer.close()
+	#with open("./output/output.txt",'wb+') as writer:
+	#	writer.write(res)
+	return
+
+def getGraph(filename):
+	vc= cv2.VideoCapture(filename)
+	if vc.isOpened():
+		rval,frame=vc.read()
+		cv2.imwrite("./output/"+str(0)+".png",frame)#第一帧
+	else:
+		rval=False
+	fps = 3
+	k = 1
+	count = 1
+	while rval:
+		rval,frame=vc.read()
+		if frame is None:
+			break
+		if(k%fps==0):
+			cv2.imwrite("./output/"+str(count)+".png",frame)
+			count += 1
+		k += 1
+		#cv2.waitKey(1)
+	vc.release()
+	return count
+
+if __name__ == "__main__":
+	#count = getGraph("./video/in.mp4")
+	#count=8
+	#i = 7
+	#while i<count:
+	#	img=cv2.imread("./output/"+str(i)+".png")
+	#	img,contours,hierachy = getContours(img)
+	#	img=find(img,contours,np.squeeze(hierachy))
+	#	cv2.imwrite("output.png",img)
+	#	print(count-i)
+	#	decode(img)
+	#	i+=1
+	img=cv2.imread("./video/0.png")
+	img,contours,hierachy = getContours(img)
+	img=find(img,contours,np.squeeze(hierachy))
+	cv2.imwrite("output.png",img)
+	decode(img)
---
+++ encodeCL.py
@@ -0,0 +1,187 @@
+import cv2
+import numpy as np
+import struct
+import os
+import sys
+border = 4
+width = 80
+inWidth = 40
+locWidth = 16
+blackWhite = 14
+
+sLocWidth = 8
+sblackWhite = 7
+
+def drawLocPoint(mat):
+	#创建定位点
+	for i in range(border,border+blackWhite):
+		#左上角
+		mat[i][border] = 0		#竖
+		mat[i][border+blackWhite-1] = 0
+		mat[i][border+1] = 0		
+		mat[i][border+blackWhite-1-1] = 0
+		mat[border][i] = 0		#横
+		mat[border+blackWhite-1][i] = 0
+		mat[border+1][i] = 0		#横
+		mat[border+blackWhite-1-1][i] = 0
+		#左下角
+		mat[width-border-blackWhite][i] = 0
+		mat[width-border-1][i] = 0
+		mat[width-border-blackWhite+1][i] = 0
+		mat[width-border-1-1][i] = 0
+		mat[width-i-1][border] = 0
+		mat[width-i-1][border+blackWhite-1] = 0
+		mat[width-i-1][border+1] = 0
+		mat[width-i-1][border+blackWhite-1-1] = 0
+		#右上角和左上角对称
+		mat[i][width-border-blackWhite] = 0
+		mat[i][width-border-1] = 0
+		mat[i][width-border-blackWhite+1] = 0
+		mat[i][width-border-1-1] = 0
+		mat[border][width-i-1] = 0
+		mat[border+blackWhite-1][width-i-1] = 0
+		mat[border+1][width-i-1] = 0
+		mat[border+blackWhite-1-1][width-i-1] = 0
+	for i in range(border,border+sblackWhite):
+		#右下角
+		mat[width-i-1][width-border-1] = 0		#竖
+		mat[width-i-1][width-(border+sblackWhite)] = 0
+		mat[width-border-1][width-i-1] = 0		#横
+		mat[width-(border+sblackWhite)][width-i-1] = 0
+
+	for i in range(border+4,border+4+6):
+		for j in range(border+4,border+4+6):
+			mat[i][j] = 0
+			mat[i][width-j-1] = 0
+			mat[width-j-1][i] = 0
+
+	for i in range(border+2,border+2+3):
+		for j in range(border+2,border+2+3):
+			mat[width-i-1][width-j-1] = 0
+	return
+
+def mask(mat,row,col):
+	if(mat[row][col]==255):
+		mat[row][col] = 0
+	else:
+		mat[row][col] = 255
+def encode(mat,binstring):
+	row=border #记录绘制到第几行
+	col=border + locWidth #
+	while binstring and row < width - border:
+		if row<border+locWidth:
+			if int(binstring[0]) == 1:
+				mat[row][col] = 0
+			if (col+row)% 2==0:
+				mask(mat,row,col)
+			col += 1
+			if col>width-border-locWidth-1:
+				if row!=border+locWidth-1 :
+					col = border + locWidth
+				else:
+					col = border
+				row += 1
+			binstring=binstring[1:]
+		elif row<width-border-locWidth:
+			if int(binstring[0]) == 1:
+				mat[row][col] = 0
+			if (col+row)% 2==0:
+				mask(mat,row,col)
+			col += 1
+			if col>width-border-1:
+				if row != width-border-locWidth-1:
+					col = border
+				else:
+					col = border + locWidth
+				row += 1
+			binstring=binstring[1:]
+		elif row < width-border-sLocWidth:
+			if int(binstring[0]) == 1:
+				mat[row][col] = 0
+			if (col+row)% 2==0:
+				mask(mat,row,col)
+			col += 1
+			if col>width-border-1:
+				col = border + locWidth
+				row += 1
+			binstring=binstring[1:]
+		else:
+			if int(binstring[0]) == 1:
+				mat[row][col] = 0
+			if (col+row)% 2==0:
+				mask(mat,row,col)
+			col += 1
+			if col>width-sLocWidth-border-1:
+				col = border + locWidth
+				row += 1
+			binstring=binstring[1:]
+	#print(row)
+	#print(bitstring)
+	#res=""
+	#while bitstring:
+	#	t = (int(bitstring[:8],2))
+	#	res += chr(t)
+	#	bitstring = bitstring[8:]
+	#print(res)
+	return binstring
+
+def drawPoint():
+	return
+
+def genImage(mat,width,filename):
+	img = np.zeros((width,width,1),dtype=np.uint8)
+	pwidth = width/len(mat)#pwidth一般取10
+	for i in range(width):
+		normali = i/pwidth
+		for j in range(width):
+			normalj = j/pwidth
+			if(normali<len(mat) and normalj<len(mat)):
+				img[i][j][0]=int(mat[int(normali)][int(normalj)])
+	cv2.imwrite(filename,img)
+	return
+
+def imgToVideo(outputFileName,num):
+	fps=20#视频帧数
+	size =(width*10,width*10)#需要转为视频的图片的尺寸
+	video = cv2.VideoWriter(outputFileName,cv2.VideoWriter_fourcc('M','J','P','G'),fps,size)
+
+	for i in range(num):
+		img=cv2.imread("./video/"+str(i)+".png")
+		video.write(img)
+
+def main(argv):
+	#mat=np.zeros((120,120),dtype = np.uint8)
+	#genImage(mat,width*10,1)
+	inputFileName = "./data/in.bin"
+	outputFileName = "./video/in.avi"
+	if len(argv)>1:
+		inputFileName = argv[1]
+		outputFileName = argv[2]
+	with open(inputFileName,'rb') as reader:
+		data=reader.read()
+	#data = ("Gettysburg Address"
+	#		"Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal. "
+	#		"Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live. It is altogether fitting and proper that we should do this. "
+	#		"But, in a larger sense, we can not dedicate -- we can not consecrate -- we can not hallow -- this ground. The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us -- that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion -- that we here highly resolve that these dead shall not have died in vain -- that this nation, under God, shall have a new birth of freedom -- and that government of the people, by the people, for the people, shall not perish from the earth."
+	#		"Abraham Lincoln"
+	#		"November 19, 1863")
+
+	binstring=""
+	for ch in data:
+		#print(struct.unpack('B',ch))
+		binstring += '{:08b}'.format(ch)
+
+	num = 0
+	while(binstring):
+		mat = [[255 for i in range(width)]for j in range(width)]
+		drawLocPoint(mat)
+		binstring=encode(mat,binstring)
+		genImage(mat,width*10,"./video/"+str(num)+".png")
+		num+=1
+		print(len(binstring))
+	#imgToVideo("./video/in.avi",num)
+
+if __name__=="__main__":
+	main(sys.argv)
+	#num = 165
+	#imgToVideo("./video/in.avi",num)
